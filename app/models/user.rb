cbcf65405bedd18fb3a67af609efcb3a263b72e1
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2023-05-02 20:02:49 -0600

    model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :last_name, :first_name, :gender, presence: true
+    validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
+    validates :gender, presence: true, inclusion: { in: %w(male female nonbinary) }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Must be valid email address' }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,10 +46,12 @@ ActiveRecord::Schema.define(version: 2023_05_03_013302) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.integer "age"
     t.string "gender"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
