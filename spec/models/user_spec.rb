2daf703f874b891ae269817eb1b5d02bea2b7d1d
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2023-05-02 20:14:55 -0600

    delete some tests set up with scaffold, create user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :gender }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :age }
+    it { should validate_numericality_of(:age).is_greater_than_or_equal_to(18) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    #it { should validate_presence_of :password_digest }
+    it { should have_secure_password }
+  end
 end
