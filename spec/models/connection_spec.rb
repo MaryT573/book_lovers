8740271fa53f8e3415e6e3ec7fbcb637afe10715
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2023-05-02 20:42:14 -0600

    model and tests connections

--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Connection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationship' do
+    it { should belong_to :user }
+    it { should belong_to(:friend).class_name('User')}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :time }
+    it { should validate_presence_of :friend_id }
+  end
 end
