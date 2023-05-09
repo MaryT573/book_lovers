aed660a71344ae6215ebbe924655043410ad6b28
Anna Marie Sterling <sterling.am@gmail.com>
2023-05-09 14:43:34 -0600

    Fix book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :book do
     title { Faker::Book.title }
-    summary { Faker::Book.author }
-    author { Faker::Number.within(range: 18..60) }
+    summary { Faker::Lorem.paragraph }
+    author { Faker::Book.author }
     publish_date { Faker::Date.backward(days: 25_550) }
   end
 end
